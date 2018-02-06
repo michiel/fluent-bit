2b723347447daa06355e6d9a4e8c8ccb31c857a9
Michiel Kalkman <mkalkman@shelde.com>
2018-02-06 03:25:04 +0000

    Plugin: Add base for nest filter

---
+++ plugins/filter_nest/nest.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2015-2018 Treasure Data Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef FLB_FILTER_NEST_H
+#define FLB_FILTER_NEST_H
+
+struct nest_ctx {
+    struct mk_list rules;
+};
+
+struct nest_rule {
+    int type;
+    char *field;
+    char *regex;
+    regex_t match;
+    struct mk_list _head;
+};
+
+#endif
